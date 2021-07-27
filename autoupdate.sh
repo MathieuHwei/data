5d3a6f4eef885c1b85e6f99422217f4d937f1f88
Mathieu Hwei <mathieuhwei@gmail.com>
2021-07-27 06:00:53 +0300

    Test if whether delete files can free up lfs space

---
+++ autoupdate.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+DATADIR="/home/pi/data"
+cd $DATADIR
+
+sudo chown -R "${USER:-$(id -un)}" .
+echo "Pull from git repository..."
+git pull
+
+if [[ -n $(git status -s) ]]; then
+    if [[ -n $(git status --porcelain|grep '2021') ]]; then
+	echo "Files change: Correct year. Pushing changes..."
+	git add 2021*  
+        git commit -m "$1: Update files " 
+        git push origin main
+    else
+	echo "Discard file with wrong years"
+	find . -not -path '*/\.*' -type f -name "2025*" -delete
+	find . -not -path '*/\.*' -type f -name "2024*" -delete
+	find . -not -path '*/\.*' -type f -name "2023*" -delete
+	find . -not -path '*/\.*' -type f -name "2022*" -delete
+    fi
+else
+    echo "No changes found. Skip pushing."
+fi
